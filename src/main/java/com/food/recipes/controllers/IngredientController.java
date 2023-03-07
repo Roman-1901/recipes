4c5fb1c4bf35be1ea0a676a70fd60483c6b5e79d
roman <roman-1901@mail.ru>
2023-03-07 21:55:42 +0300

    done

--- src/main/java/com/food/recipes/controllers/IngredientController.java
+++ src/main/java/com/food/recipes/controllers/IngredientController.java
@@ -1,6 +1,5 @@
 package com.food.recipes.controllers;
 import com.food.recipes.model.Ingredient;
-import com.food.recipes.model.Recipe;
 import com.food.recipes.services.IngredientService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
